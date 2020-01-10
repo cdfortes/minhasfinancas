85d636be196296c531d2483158004341e1bf595d
cdfortes <cdfortes@gmail.com>
2020-01-10 11:33:42 -0100

    first commit

---
+++ src/test/java/cv/cdfortes/minhasfinancas/MinhasfinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package cv.cdfortes.minhasfinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhasfinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
